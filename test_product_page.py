f75f45f8a461832e969798e86fba131c2c7e4551
almat3010 <almat.sultanov@gmail.com>
2023-02-06 19:46:24 +0300

    add parametrize for product page

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,8 @@
+import pytest
 from .pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_basket(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+@pytest.mark.parametrize('url', ["http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209","http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"])
+def test_guest_can_add_product_to_basket(browser,url):
     browser.implicitly_wait(15)
     page = ProductPage(browser, url)
     page.open()
