8988b467ddee720d4ffc02fac6569df0ac5c1ffb
almat3010 <almat.sultanov@gmail.com>
2023-02-06 17:18:22 +0300

    exception handling for login-link

--- Pages/Base_Page.py
+++ Pages/Base_Page.py
@@ -1,6 +1,14 @@
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
     def open(self):
         self.browser.get(self.url)
+    def is_element_present(self, by, selector):
+        try:
+            self.browser.find_element(by,selector)
+        except(NoSuchElementException):
+            return False
+        return True
