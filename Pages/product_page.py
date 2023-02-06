37d6702915c437d100bf7073a7a4ecee295e3a61
almat3010 <almat.sultanov@gmail.com>
2023-02-06 20:12:11 +0300

    xfail-mark for bugged url

--- Pages/product_page.py
+++ Pages/product_page.py
@@ -4,13 +4,11 @@ from .locators import ProductPageLocators
 class ProductPage(BasePage):
     def add_to_basket(self):
         self.click(*ProductPageLocators.BUTTON_ADD_BASKET)
-        #solve = self.solve_quiz_and_get_code()
+        self.solve_quiz_and_get_code()
 
     def should_be_add_in_basket(self):
         msg_add_to_basket = self.browser.find_element(*ProductPageLocators.ALERT_INNER)
         title_of_book = self.browser.find_element(*ProductPageLocators.TITLE_OF_BOOK)
-        print(msg_add_to_basket.text)
-        print(title_of_book.text)
         assert msg_add_to_basket.text == title_of_book.text, "title is not same"
 
     def should_be_same_price(self):
